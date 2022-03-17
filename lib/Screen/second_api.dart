9d20a3deb33720fa4fab6e7fa0b19765872e0c87
rabbihossen <joyahmedauthor@gmail.com>
2022-03-17 22:54:09 +0600

    second api done

--- lib/Screen/second_api.dart
+++ lib/Screen/second_api.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import '../Model/product_model.dart';
+import '../Model/second_model.dart';
 
 class SecondApi extends StatefulWidget {
   const SecondApi({Key? key}) : super(key: key);
@@ -10,7 +11,7 @@ class SecondApi extends StatefulWidget {
 
 class _SecondApiState extends State<SecondApi> {
   String outputText = 'Output';
-  List<Products> productList = [];
+  List<Welcome> productList = [];
 
   @override
   void initState() {
@@ -35,37 +36,28 @@ class _SecondApiState extends State<SecondApi> {
               ),
               itemCount: productList.length,
               itemBuilder: (context, index) {
-                return InkWell(
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (
-                      BuildContext context,
-                    ) =>
-                            Dtails(productList)));
-                  },
-                  child: Card(
-                    shadowColor: Colors.green,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Expanded(
-                            flex: 3,
-                            child: Image.network(
-                              productList[index].image,
-                              height: 150,
-                              width: 180,
-                            )),
-                        Expanded(
-                            flex: 2,
-                            child: Text(
-                              productList[index].title,
-                              style: TextStyle(fontSize: 15),
-                            )),
-                        Expanded(
-                            flex: 1,
-                            child: Text(productList[index].price.toString())),
-                      ],
-                    ),
+                return Card(
+                  shadowColor: Colors.green,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                          flex: 3,
+                          child: Image.network(
+                            productList[index].image,
+                            height: 150,
+                            width: 180,
+                          )),
+                      Expanded(
+                          flex: 2,
+                          child: Text(
+                            productList[index].title,
+                            style: TextStyle(fontSize: 15),
+                          )),
+                      Expanded(
+                          flex: 1,
+                          child: Text(productList[index].price.toString())),
+                    ],
                   ),
                 );
               },
@@ -79,12 +71,12 @@ class _SecondApiState extends State<SecondApi> {
   Future<void> functions() async {
     var response = await http
         .get(
-      Uri.parse('https://fakestoreapi.com/products'),
+      Uri.parse('https://fakestoreapi.com/products/category/jewelery'),
     )
         .then((value) {
       setState(() {
         if (value.statusCode == 200) {
-          List<Products> pl = productsFromJson(value.body);
+          List<Welcome> pl = welcomeFromJson(value.body);
           for (var i = 0; i < pl.length; i++) {
             productList.add(pl[i]);
           }
